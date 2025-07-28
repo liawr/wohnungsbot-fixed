58d1ecdcd18e2e80533f2155033db9e3afd5f968
neopostmodern <clemens@neopostmodern.com>
2025-07-28 18:06:02 +0200

    fix: adapt to new flat site navigation, closes #225
    - catch URLs of flats opening in new tabs and open them directly
    - update selectors

--- app/constants/querySelectors.ts
+++ app/constants/querySelectors.ts
@@ -1,7 +1,7 @@
 export const entrySelector = (entryId: string): string =>
-  `[data-id="${entryId}"]`;
+  `[data-obid="${entryId}"]`;
 export const entryTitleSelector = (entryId: string): string =>
-  `.result-list-entry__data a[data-exp-id="${entryId}"] > h2`;
+  `a[data-exp-id="${entryId}"] h2`;
 
 export const composeMessageButtonSelector = '[data-qa="sendButton"]';
 
